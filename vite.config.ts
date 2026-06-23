b253dbcf856372d4360390de94a662a17541463d
Omer YIGITLER <138784969+omeryigitler@users.noreply.github.com>
2026-06-23 05:19:55 +0200

    chore: remove build-time app patch from vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
-import { patchOlinkbuApp } from './src/build/patchOlinkbuApp';
 
 export default defineConfig(() => {
   return {
-    plugins: [patchOlinkbuApp(), react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
